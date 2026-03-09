413f624a3a0d8af7e0d998e0b5b69a0b716bd495
Michael Vogel <michael.vogel@flagbit.de>
2026-03-09 19:42:02 +0100

    feat: Add Requirements

--- src/Loader/LDCadLoader.php
+++ src/Loader/LDCadLoader.php
@@ -7,7 +7,7 @@ use Vogaeael\RebrickablePartListsComparer\Model\LDCadPart;
 
 class LDCadLoader extends AbstractLoader
 {
-    private const PART_REGEX = '/([\da-z]+).dat:\[sourceInv=([a-z]+)\] \[color=(\d+)\] \[count=(\d+)\]/';
+    private const PART_REGEX = '/([\da-z]+).dat:\[sourceInv=([a-z]+)] \[color=(\d+)] \[count=(\d+)]/';
 
     /**
      * @throws Exception
