f1fadb6c107f5565e590d8dada32e2503807f1f3
Michael Vogel <michael.vogel@flagbit.de>
2026-03-10 23:56:14 +0100

    test: Add unit test for ldcad loader

---
+++ tests/Loader/LDCadLoaderTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vogaeael\tests\Loader;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Loader\LDCadLoader;
+use Vogaeael\RebrickablePartListsComparer\Model\LDCadPart;
+
+class LDCadLoaderTest extends TestCase
+{
+    public function testLoad(): void
+    {
+        $loader = new LDCadLoader();
+        $actual = $loader->load(__DIR__ . '/../data/ldcad_parts.pbg');
+
+        $this->assertIsArray($actual);
+        $this->assertCount(3, $actual);
+
+        $part = new LDCadPart('99781', '72', 'parts', 5);
+        $this->assertContainsEquals($part, $actual);
+
+        $part = new LDCadPart('7504pr0001', '0', 'parts', 2);
+        $this->assertContainsEquals($part, $actual);
+
+        $part = new LDCadPart('3010', '15', 'parts', 12);
+        $this->assertContainsEquals($part, $actual);
+    }
+}
