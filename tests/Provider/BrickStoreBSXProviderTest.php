4ceb85752369e028ffc1880b73376ddd59e3c57a
Michael Vogel <michael.vogel@flagbit.de>
2026-03-11 15:10:04 +0100

    test: Add unit test for brick-store-bsx provider

---
+++ tests/Provider/BrickStoreBSXProviderTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Vogaeael\tests\Provider;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Helper\CheckClass;
+use Vogaeael\RebrickablePartListsComparer\Loader\BrickStoreBSXLoader;
+use Vogaeael\RebrickablePartListsComparer\Provider\BrickStoreBSXProvider;
+use Vogaeael\RebrickablePartListsComparer\Subtractor\BrickStoreBSXSubtractor;
+use Vogaeael\RebrickablePartListsComparer\Types;
+use Vogaeael\RebrickablePartListsComparer\Writer\BrickStoreBSXWriter;
+
+class BrickStoreBSXProviderTest extends TestCase
+{
+    public function testGetLoader(): void
+    {
+        $provider = new BrickStoreBSXProvider($this->createStub(CheckClass::class));
+
+        $loaderA = $provider->getLoader();
+        $this->assertInstanceOf(BrickStoreBSXLoader::class, $loaderA);
+    }
+
+    public function testGetWriter(): void
+    {
+        $provider = new BrickStoreBSXProvider($this->createStub(CheckClass::class));
+
+        $writerA = $provider->getWriter();
+        $this->assertInstanceOf(BrickStoreBSXWriter::class, $writerA);
+    }
+
+    public function testGetSubtractor(): void
+    {
+        $provider = new BrickStoreBSXProvider($this->createStub(CheckClass::class));
+
+        $subtractorA = $provider->getSubtractor();
+        $this->assertInstanceOf(BrickStoreBSXSubtractor::class, $subtractorA);
+    }
+
+    public function testGetKey(): void
+    {
+        $provider = new BrickStoreBSXProvider($this->createStub(CheckClass::class));
+
+        $this->assertSame(Types::BrickStoreBSX, $provider->getKey());
+    }
+}
