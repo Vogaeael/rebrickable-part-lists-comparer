93201608647b93be7d5933a430458bc04e92b6ec
Michael Vogel <michael.vogel@flagbit.de>
2026-03-11 03:27:43 +0100

    test: Add unit test for brick-link provider

---
+++ tests/Provider/BrickLinkProviderTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Vogaeael\tests\Provider;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Helper\CheckClass;
+use Vogaeael\RebrickablePartListsComparer\Loader\BrickLinkLoader;
+use Vogaeael\RebrickablePartListsComparer\Provider\BrickLinkProvider;
+use Vogaeael\RebrickablePartListsComparer\Subtractor\BrickLinkSubtractor;
+use Vogaeael\RebrickablePartListsComparer\Types;
+use Vogaeael\RebrickablePartListsComparer\Writer\BrickLinkWriter;
+
+class BrickLinkProviderTest extends TestCase
+{
+    public function testGetLoader(): void
+    {
+        $provider = new BrickLinkProvider($this->createStub(CheckClass::class));
+
+        $loaderA = $provider->getLoader();
+        $this->assertInstanceOf(BrickLinkLoader::class, $loaderA);
+    }
+
+    public function testGetWriter(): void
+    {
+        $provider = new BrickLinkProvider($this->createStub(CheckClass::class));
+
+        $writerA = $provider->getWriter();
+        $this->assertInstanceOf(BrickLinkWriter::class, $writerA);
+    }
+
+    public function testGetSubtractor(): void
+    {
+        $provider = new BrickLinkProvider($this->createStub(CheckClass::class));
+
+        $subtractorA = $provider->getSubtractor();
+        $this->assertInstanceOf(BrickLinkSubtractor::class, $subtractorA);
+    }
+
+    public function testGetKey(): void
+    {
+        $provider = new BrickLinkProvider($this->createStub(CheckClass::class));
+
+        $this->assertSame(Types::BrickLink, $provider->getKey());
+    }
+}
