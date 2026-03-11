78d11f195ffdf7c2ca329262eaf44cacf78b5a28
Michael Vogel <michael.vogel@flagbit.de>
2026-03-11 02:46:34 +0100

    test: Add unit test for ldcad part

---
+++ tests/Model/LDCadPartTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Vogaeael\tests\Model;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Model\LDCadPart;
+
+class LDCadPartTest extends TestCase
+{
+    public static function additionProvider(): array
+    {
+        return [
+            [
+                'params' => [
+                    'identifier1',
+                    'color1',
+                    'sourceInv1',
+                    1,
+                ],
+                'results' => [
+                    'identifier' => 'identifier1',
+                    'color' => 'color1',
+                    'sourceInv' => 'sourceInv1',
+                    'quantity' => 1,
+                    'key' => 'i:identifier1-c:color1',
+                ],
+            ],
+            [
+                'params' => [
+                    'identifier2',
+                    'color2',
+                    'sourceInv2',
+                    2,
+                ],
+                'results' => [
+                    'identifier' => 'identifier2',
+                    'color' => 'color2',
+                    'sourceInv' => 'sourceInv2',
+                    'quantity' => 2,
+                    'key' => 'i:identifier2-c:color2',
+                ],
+            ],
+            [
+                'params' => [
+                    'identifier3',
+                    'color3',
+                    'sourceInv3',
+                    3,
+                ],
+                'results' => [
+                    'identifier' => 'identifier3',
+                    'color' => 'color3',
+                    'sourceInv' => 'sourceInv3',
+                    'quantity' => 3,
+                    'key' => 'i:identifier3-c:color3',
+                ],
+            ],
+        ];
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testGetQuantity(array $params, array $results): void
+    {
+        $part = new LDCadPart(...$params);
+
+        $this->assertSame($results['quantity'], $part->quantity);
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testSetQuantity(array $params, array $results): void
+    {
+        $this->expectException(\Error::class);
+        $part = new LDCadPart(...$params);
+
+        $part->quantity = 1;
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testGetIdentifier(array $params, array $results): void
+    {
+        $part = new LDCadPart(...$params);
+
+        $this->assertSame($results['identifier'], $part->identifier);
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testSetIdentifier(array $params, array $results): void
+    {
+        $this->expectException(\Error::class);
+        $part = new LDCadPart(...$params);
+
+        $part->identifier = 'test';
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testGetColor(array $params, array $results): void
+    {
+        $part = new LDCadPart(...$params);
+
+        $this->assertSame($results['color'], $part->color);
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testSetColor(array $params, array $results): void
+    {
+        $this->expectException(\Error::class);
+        $part = new LDCadPart(...$params);
+
+        $part->color = 'test';
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testGetSourceInv(array $params, array $results): void
+    {
+        $part = new LDCadPart(...$params);
+
+        $this->assertSame($results['sourceInv'], $part->sourceInv);
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testSetSourceInv(array $params, array $results): void
+    {
+        $this->expectException(\Error::class);
+        $part = new LDCadPart(...$params);
+
+        $part->sourceInv = 'test';
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testGenerateKey(array $params, array $results): void
+    {
+        $part = new LDCadPart(...$params);
+
+        $this->assertSame($results['key'], $part->generateKey());
+    }
+}
