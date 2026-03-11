8661c4cf8702dd7892cad47e4b708788128e1913
Michael Vogel <michael.vogel@flagbit.de>
2026-03-11 15:11:28 +0100

    test: Add unit test for ldcad provider

---
+++ tests/Provider/LDCadProviderTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Vogaeael\tests\Provider;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Helper\CheckClass;
+use Vogaeael\RebrickablePartListsComparer\Loader\LDCadLoader;
+use Vogaeael\RebrickablePartListsComparer\Provider\LDCadProvider;
+use Vogaeael\RebrickablePartListsComparer\Subtractor\LDCadSubtractor;
+use Vogaeael\RebrickablePartListsComparer\Types;
+use Vogaeael\RebrickablePartListsComparer\Writer\LDCadWriter;
+
+class LDCadProviderTest extends TestCase
+{
+    public function testGetLoader(): void
+    {
+        $provider = new LDCadProvider($this->createStub(CheckClass::class));
+
+        $loaderA = $provider->getLoader();
+        $this->assertInstanceOf(LDCadLoader::class, $loaderA);
+    }
+
+    public function testGetWriter(): void
+    {
+        $provider = new LDCadProvider($this->createStub(CheckClass::class));
+
+        $writerA = $provider->getWriter();
+        $this->assertInstanceOf(LDCadWriter::class, $writerA);
+    }
+
+    public function testGetSubtractor(): void
+    {
+        $provider = new LDCadProvider($this->createStub(CheckClass::class));
+
+        $subtractorA = $provider->getSubtractor();
+        $this->assertInstanceOf(LDCadSubtractor::class, $subtractorA);
+    }
+
+    public function testGetKey(): void
+    {
+        $provider = new LDCadProvider($this->createStub(CheckClass::class));
+
+        $this->assertSame(Types::LDCad, $provider->getKey());
+    }
+}
