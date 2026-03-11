32a2602de00235a68f80fa879e8fb39f93d89157
Michael Vogel <michael.vogel@flagbit.de>
2026-03-11 15:13:25 +0100

    test: Add unit test for lego-pick-a-brick provider

---
+++ tests/Provider/LegoPickABrickProviderTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Vogaeael\tests\Provider;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Helper\CheckClass;
+use Vogaeael\RebrickablePartListsComparer\Loader\LegoPickABrickLoader;
+use Vogaeael\RebrickablePartListsComparer\Provider\LegoPickABrickProvider;
+use Vogaeael\RebrickablePartListsComparer\Subtractor\LegoPickABrickSubtractor;
+use Vogaeael\RebrickablePartListsComparer\Types;
+use Vogaeael\RebrickablePartListsComparer\Writer\LegoPickABrickWriter;
+
+class LegoPickABrickProviderTest extends TestCase
+{
+    public function testGetLoader(): void
+    {
+        $provider = new LegoPickABrickProvider($this->createStub(CheckClass::class));
+
+        $loaderA = $provider->getLoader();
+        $this->assertInstanceOf(LegoPickABrickLoader::class, $loaderA);
+    }
+
+    public function testGetWriter(): void
+    {
+        $provider = new LegoPickABrickProvider($this->createStub(CheckClass::class));
+
+        $writerA = $provider->getWriter();
+        $this->assertInstanceOf(LegoPickABrickWriter::class, $writerA);
+    }
+
+    public function testGetSubtractor(): void
+    {
+        $provider = new LegoPickABrickProvider($this->createStub(CheckClass::class));
+
+        $subtractorA = $provider->getSubtractor();
+        $this->assertInstanceOf(LegoPickABrickSubtractor::class, $subtractorA);
+    }
+
+    public function testGetKey(): void
+    {
+        $provider = new LegoPickABrickProvider($this->createStub(CheckClass::class));
+
+        $this->assertSame(Types::LegoPickABrick, $provider->getKey());
+    }
+}
