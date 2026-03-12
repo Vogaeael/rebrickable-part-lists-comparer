8b5cf4c66591758cadfb693ec286e2fe93e96c0d
Michael Vogel <michael.vogel@flagbit.de>
2026-03-12 03:12:45 +0100

    test: Add unit test for brickstore-bsx writer

---
+++ tests/unit/Writer/BrickStoreBSXWriterTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Vogaeael\RebrickablePartListsComparer\tests\unit\Writer;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Helper\CheckClass;
+use Vogaeael\RebrickablePartListsComparer\Model\BrickStoreBSXPart;
+use Vogaeael\RebrickablePartListsComparer\Writer\BrickStoreBSXWriter;
+
+class BrickStoreBSXWriterTest extends TestCase
+{
+    public function testWritePart(): void
+    {
+        $parts = [];
+        $part = new BrickStoreBSXPart('itemTypeA', 'itemIdA', 'colorIdA', 3);
+        $parts[$part->generateKey()] = $part;
+        $part = new BrickStoreBSXPart('itemTypeB', 'itemIdB', 'colorIdB', 453);
+        $parts[$part->generateKey()] = $part;
+        $part = new BrickStoreBSXPart('itemTypeC', 'itemIdC', 'colorIdC', 26);
+        $parts[$part->generateKey()] = $part;
+
+        $filePath = __DIR__ . '/../../data/write/brick_store_parts.bsx';
+
+        $writer = new BrickStoreBSXWriter(new CheckClass());
+        $writer->writePart($parts, $filePath);
+
+        $expected = '<BrickStoreXML><Inventory><Item>';
+        $expected .= '<ItemTypeID>itemTypeA</ItemTypeID>';
+        $expected .= '<ItemID>itemIdA</ItemID>';
+        $expected .= '<ColorID>colorIdA</ColorID>';
+        $expected .= '<Qty>3</Qty>';
+        $expected .= '</Item><Item>';
+        $expected .= '<ItemTypeID>itemTypeB</ItemTypeID>';
+        $expected .= '<ItemID>itemIdB</ItemID>';
+        $expected .= '<ColorID>colorIdB</ColorID>';;
+        $expected .= '<Qty>453</Qty>';
+        $expected .= '</Item><Item>';
+        $expected .= '<ItemTypeID>itemTypeC</ItemTypeID>';
+        $expected .= '<ItemID>itemIdC</ItemID>';
+        $expected .= '<ColorID>colorIdC</ColorID>';
+        $expected .= '<Qty>26</Qty>';
+        $expected .= '</Item></Inventory></BrickStoreXML>';
+
+        $actual = file_get_contents($filePath);
+        $this->assertEquals($expected, $actual);
+
+        unlink($filePath);
+    }
+}
