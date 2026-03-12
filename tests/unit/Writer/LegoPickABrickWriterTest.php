f28fc746e92a60c638c264173a3c1f764fe700e7
Michael Vogel <michael.vogel@flagbit.de>
2026-03-12 03:36:43 +0100

    test: Add unit test for lego-pab writer

---
+++ tests/unit/Writer/LegoPickABrickWriterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vogaeael\RebrickablePartListsComparer\tests\unit\Writer;
+
+use PHPUnit\Framework\TestCase;
+use Vogaeael\RebrickablePartListsComparer\Helper\CheckClass;
+use Vogaeael\RebrickablePartListsComparer\Model\LegoPickABrickPart;
+use Vogaeael\RebrickablePartListsComparer\Writer\LegoPickABrickWriter;
+
+class LegoPickABrickWriterTest extends TestCase
+{
+    public function testWritePart(): void
+    {
+        $parts = [];
+        $part = new LegoPickABrickPart('elementIdA', 3);
+        $parts[$part->generateKey()] = $part;
+        $part = new LegoPickABrickPart('elementIdB', 453);
+        $parts[$part->generateKey()] = $part;
+        $part = new LegoPickABrickPart('elementIdC', 26);
+        $parts[$part->generateKey()] = $part;
+
+        $filePath = __DIR__ . '/../../data/write/lego_pab_parts.csv';
+
+        $writer = new LegoPickABrickWriter(new CheckClass());
+        $writer->writePart($parts, $filePath);
+
+        $expected = 'elementId,quantity' . PHP_EOL;
+        $expected .= 'elementIdA,3' . PHP_EOL;
+        $expected .= 'elementIdB,453' . PHP_EOL;
+        $expected .= 'elementIdC,26' . PHP_EOL;
+
+        $actual = file_get_contents($filePath);
+        $this->assertEquals($expected, $actual);
+
+        unlink($filePath);
+    }
+}
